9aa8a1945469c81623834e57ce6c51301c8a367d
Lucas Oshiro <lucasseikioshiro@gmail.com>
2025-02-07 12:12:21 -0300

    feat: add thread support

--- model/libiec61850/hal/thread/ns3/thread.cc
+++ model/libiec61850/hal/thread/ns3/thread.cc
@@ -1,48 +1,83 @@
-#include "ns3/hal_thread.h"
 #include "ns3/core-module.h"
+#include "ns3/hal_thread.h"
 
-const size_t semaphore_size = 32;
+constexpr size_t semaphore_size = 32;
 typedef std::counting_semaphore<semaphore_size> std_semaphore;
 
 namespace libiec61850
 {
 
-/**
- * \brief Create a new Thread instance
- *
- * \param function the entry point of the thread
- * \param parameter a parameter that is passed to the threads start function
- * \param autodestroy the thread is automatically destroyed if the ThreadExecutionFunction has finished.
- *
- * \return the newly created Thread instance
- */
+struct sThread {
+    ThreadExecutionFunction function;
+    void* parameter;
+    std::thread* thread;
+    bool state;
+    bool autodestroy;
+};
+
+static void threadJob(Thread thread)
+{
+    thread->function(thread->parameter);
+}
+
+static void threadJobDestroyAutomatic(Thread thread)
+{
+    thread->function(thread->parameter);
+    delete thread;
+}
+
 PAL_API Thread
-Thread_create(ThreadExecutionFunction function, void* parameter, bool autodestroy);
+Thread_create(ThreadExecutionFunction function, void* parameter, bool autodestroy)
+{
+    auto thread = new struct sThread;
 
-/**
- * \brief Start a Thread.
- *
- * This function invokes the start function of the thread. The thread terminates when
- * the start function returns.
- *
- * \param thread the Thread instance to start
- */
-PAL_API void
-Thread_start(Thread thread);
+    if (thread != nullptr)
+    {
+        thread->parameter = parameter;
+        thread->function = function;
+        thread->state = false;
+        thread->autodestroy = autodestroy;
+        thread->thread = nullptr;
+    }
 
-/**
- * \brief Destroy a Thread and free all related resources.
- *
- * \param thread the Thread instance to destroy
- */
-PAL_API void
-Thread_destroy(Thread thread);
+    return thread;
+}
 
-/**
- * \brief Suspend execution of the Thread for the specified number of milliseconds
- */
 PAL_API void
-Thread_sleep(int millies);
+Thread_start(Thread thread)
+{
+
+    if (thread->autodestroy)
+    {
+        thread->thread = new std::thread(threadJobDestroyAutomatic, thread);
+        thread->thread->detach();
+    }
+    else
+    {
+        thread->thread = new std::thread(threadJob, thread);
+    }
+}
+
+PAL_API void
+Thread_destroy(Thread thread)
+{
+    if (thread->state)
+    {
+        thread->thread->join();
+    }
+
+    delete thread->thread;
+    delete thread;
+}
+
+PAL_API void
+Thread_sleep(int millies)
+{
+    /* TODO: how do we implement an sleep in terms of simulation? Perhaps we
+     * can use the Yoneda lemma to transform a sleep into a schedule?
+     */
+    (void) millies;
+}
 
 PAL_API Semaphore
 Semaphore_create(int initialValue)
@@ -50,7 +85,6 @@ Semaphore_create(int initialValue)
     return new std::counting_semaphore<semaphore_size>(initialValue);
 }
 
-/* Wait until semaphore value is greater than zero. Then decrease the semaphore value. */
 PAL_API void
 Semaphore_wait(Semaphore self)
 {
