3e400ddcde35d44d23ad9282b49d9ddff99d9872
Lucas Oshiro <lucasseikioshiro@gmail.com>
2025-03-13 16:30:08 -0300

    feat: add SVPublisher class

--- model/sv-subscriber.cc
+++ model/sv-subscriber.cc
@@ -1,5 +1,29 @@
-//
-// Created by lso on 13/03/25.
-//
-
 #include "sv-subscriber.h"
+
+ns3::SVSubscriber::
+SVSubscriber()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+ns3::TypeId
+ns3::SVSubscriber::GetTypeId()
+{
+    static TypeId tid =TypeId("ns3::SVSubscriber")
+       .SetParent<Application>()
+       .SetGroupName("LibIEC61850")
+       .AddConstructor<SVSubscriber>();
+    return tid;
+}
+
+void
+ns3::SVSubscriber::StartApplication()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+void
+ns3::SVSubscriber::StopApplication()
+{
+    NS_LOG_FUNCTION(this);
+}
--- model/sv-subscriber.h
+++ model/sv-subscriber.h
@@ -1,8 +1,22 @@
-//
-// Created by lso on 13/03/25.
-//
-
 #ifndef SV_SUBSCRIBER_H
 #define SV_SUBSCRIBER_H
+#include "../../../src/network/model/application.h"
+
+namespace ns3
+{
+
+class SVSubscriber : public Application
+{
+public:
+    SVSubscriber();
+    static TypeId GetTypeId();
+
+private:
+    void StartApplication() override;
+    void StopApplication() override;
+};
+
+
+}
 
 #endif //SV_SUBSCRIBER_H
