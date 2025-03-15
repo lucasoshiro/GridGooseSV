14673b00baad156b45efdaf41d01ce31dd56a618
Lucas Oshiro <lucasseikioshiro@gmail.com>
2025-03-14 22:41:56 -0300

    feat: make sv subscriber test behave like libiec61850 sv-subscriber example

--- model/sv-subscriber.cc
+++ model/sv-subscriber.cc
@@ -1,19 +1,28 @@
 #include "sv-subscriber.h"
-
 #include "ns3/sv_subscriber.h"
 
-#include <fstream>
-
-static void
-svUpdateListener(
-    libiec61850::SVSubscriber subscriber,
-    void* parameter,
+static void svUpdateListener(
+    libiec61850::SVSubscriber libiecSubscriber,
+    void *ns3Subscriber,
     libiec61850::SVSubscriber_ASDU asdu
     )
 {
-    std::ofstream output("/dev/pts/1");
-    output << "YEYEYEYEYEYEYEYE" << std::endl;
-    output.close();
+    // std::ofstream output("/dev/pts/1");
+    auto &output = std::cout;
+    output << "svUpdateListener called" << std::endl;
+
+    auto subscriber = static_cast<ns3::SVSubscriber*>(ns3Subscriber);
+
+    auto svID = libiec61850::SVSubscriber_ASDU_getSvId(asdu);
+
+    output << "  svID: " << svID << std::endl;
+    output << "  smpCnt: " << libiec61850::SVSubscriber_ASDU_getSmpCnt(asdu) << std::endl;
+    output << "  confRev: " << libiec61850::SVSubscriber_ASDU_getConfRev(asdu)<< std::endl;
+    output << "  DATA[0]: " << libiec61850::SVSubscriber_ASDU_getFLOAT32(asdu, 0) << std::endl;
+    output << "  DATA[1]: " << libiec61850::SVSubscriber_ASDU_getFLOAT32(asdu, 4) << std::endl;
+    output << std::endl;
+
+    //output.close();
 }
 
 ns3::SVSubscriber::
@@ -47,10 +56,10 @@ ns3::SVSubscriber::StartApplication()
 
     //TODO: set appID as a parameter
     this->subscriber = libiec61850::SVSubscriber_create(nullptr, 0x4000);
-//    libiec61850::SVSubscriber_setListener(subscriber, svUpdateListener, nullptr);
+    libiec61850::SVSubscriber_setListener(subscriber, svUpdateListener, this);
 
     libiec61850::SVReceiver_addSubscriber(receiver, subscriber);
-    //libiec61850::SVReceiver_start(receiver);
+    libiec61850::SVReceiver_start(receiver);
 }
 
 void
