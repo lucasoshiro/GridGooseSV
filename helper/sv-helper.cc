3931c0b8f02136d6fb67dfebb14a1057e36fb309
Lucas Oshiro <lucasseikioshiro@gmail.com>
2025-03-14 23:02:04 -0300

    feat: add SV subscriber helper

--- helper/sv-helper.cc
+++ helper/sv-helper.cc
@@ -1,7 +1,15 @@
 #include "sv-helper.h"
+#include "ns3/sv-publisher.h"
+#include "ns3/sv-subscriber.h"
 
 ns3::SVPublisherHelper::
 SVPublisherHelper()
-: ApplicationHelper(ns3::SVPublisher::GetTypeId())
+    : ApplicationHelper(ns3::SVPublisher::GetTypeId())
+{
+}
+
+ns3::SVSubscriberHelper::
+SVSubscriberHelper()
+    : ApplicationHelper(ns3::SVSubscriber::GetTypeId())
 {
 }
